2e5a6bf1361e7661504126f46fc2bb239c08a0d7
Stephon Fonrose <stephonfonrose@gmail.com>
2018-12-25 03:39:56 -0500

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Stephon Fonrose on 12/25/18.
+//  Copyright © 2018 Stephon Fonrose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
